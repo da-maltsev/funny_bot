5ac3004b3469ec47092ebeaa9086f5570bab4721
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-04-14 13:48:16 +0500

    add sentry monitoring

--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 from os import getenv
+from typing import Optional
 
 from dotenv import load_dotenv
+import sentry_sdk
 
 load_dotenv()
 
@@ -11,6 +13,12 @@ class Settings:
     OPENAI_TOKEN: str = getenv("OPENAI_TOKEN", "top-secret")
     HOLIDAY_TOKEN: str = getenv("HOLIDAY_TOKEN", "top-secret")
     TELEGRAM_TOKEN: str = getenv("TELEGRAM_TOKEN", "top-secret")
+    SENTRY_DSN: Optional[str] = getenv("SENTRY_DSN", None)
 
 
 settings = Settings()
+
+sentry_sdk.init(
+    dsn=settings.SENTRY_DSN,
+    traces_sample_rate=0.5,
+)
