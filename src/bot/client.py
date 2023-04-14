3e0542304d4c9c9e2b444c7315eca44cfc455cc2
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-04-14 13:33:49 +0500

    user_name injection

--- src/bot/client.py
+++ src/bot/client.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 import logging
-from typing import Callable
+from typing import Callable, Optional
 
 from telegram import Update
 from telegram.ext import ApplicationBuilder
@@ -37,7 +37,8 @@ class TelegramBot:
 
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         logging.warn("Someone started")
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{update.message.from_user.first_name}!\n{help_text.get('start', 'Hello there!')}")  # type: ignore
+        user_name = f"{self.user_name_from_update(update)}!\n" if self.user_name_from_update(update) else None
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{user_name}{help_text.get('start', 'Hello there!')}")  # type: ignore
 
     async def help(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await context.bot.send_message(
@@ -47,15 +48,18 @@ class TelegramBot:
 
     async def today_picture(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         logging.info("today picture was called")
-        await PictureOfTheDay(*self.clients, context)(update.effective_chat.id)  # type: ignore
+        user_name = self.user_name_from_update(update)
+        await PictureOfTheDay(*self.clients, context)(update.effective_chat.id, user_name)  # type: ignore
 
     async def make_picture(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         logging.info(update.message.text)  # type: ignore
-        await PictureMaker(*self.clients, context)(update.effective_chat.id, update.message.text)  # type: ignore
+        user_name = self.user_name_from_update(update)
+        await PictureMaker(*self.clients, context)(update.effective_chat.id, update.message.text, user_name)  # type: ignore
 
     async def ask_question(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         logging.info(update.message.text)  # type: ignore
-        await AskQuestionService(*self.clients, context)(update.effective_chat.id, update.message.text)  # type: ignore
+        user_name = self.user_name_from_update(update)
+        await AskQuestionService(*self.clients, context)(update.effective_chat.id, update.message.text, user_name)  # type: ignore
 
     def add_handlers(self) -> None:
         self.add_commands(
@@ -74,3 +78,10 @@ class TelegramBot:
     def add_message_nadlers(self) -> None:
         self.application.add_handler(MessageHandler(make_picture_filter & (~filters.COMMAND), self.make_picture))
         self.application.add_handler(MessageHandler(ask_question_filter & (~filters.COMMAND), self.ask_question))
+
+    @classmethod
+    def user_name_from_update(cls, update: Update) -> Optional[str]:
+        message = update.message
+        if not message:
+            return None
+        return message.from_user and message.from_user.first_name or None
