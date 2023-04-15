21e440aa09f24187d1ed789dd524219565a0de99
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-04-15 11:53:42 +0500

    Bot reacts at any register

--- src/bot/filters/filters.py
+++ src/bot/filters/filters.py
@@ -3,42 +3,42 @@ from telegram.ext.filters import MessageFilter
 
 class MakePictureFilter(MessageFilter):
     list_of_triggers = [
-        "Картинка, где",
-        "Картинка где",
-        "Бот дай картинку, где",
-        "Бот дай картинку где",
-        "Хочу картинку где",
-        "Хочу картинку, где",
+        "картинка, где",
+        "картинка где",
+        "бот дай картинку, где",
+        "бот дай картинку где",
+        "хочу картинку где",
+        "хочу картинку, где",
     ]
 
     def filter(self, message):
-        return any([message.text.startswith(expression) for expression in self.list_of_triggers])
+        return any([message.text.lower().startswith(expression) for expression in self.list_of_triggers])
 
     @classmethod
     def len_trigger(cls, text: str) -> int:  # type: ignore
         for trigger in cls.list_of_triggers:
-            if text.startswith(trigger):
+            if text.lower().startswith(trigger):
                 return len(trigger) + 1
 
 
 class AskQuestionFilter(MessageFilter):
     list_of_nice = [
-        "Бот есть вопрос",
-        "Бот ответь",
-        "Бот как думаешь",
-        "Бот скажи",
-        "Бот напиши",
+        "бот есть вопрос",
+        "бот ответь",
+        "бот как думаешь",
+        "бот скажи",
+        "бот напиши",
     ]
     list_of_toxic = [
-        "Э слыш",
-        "Слыш бот ответь",
-        "Слыш есть вопрос",
-        "Слыш скажи",
-        "Слыш ответь",
+        "э слыш",
+        "слыш бот ответь",
+        "слыш есть вопрос",
+        "слыш скажи",
+        "слыш ответь",
     ]
 
     def filter(self, message):
-        return any([message.text.startswith(expression) for expression in self.get_list_of_triggers()])
+        return any([message.text.lower().startswith(expression) for expression in self.get_list_of_triggers()])
 
     @classmethod
     def get_list_of_triggers(self) -> list[str]:
@@ -47,13 +47,13 @@ class AskQuestionFilter(MessageFilter):
     @classmethod
     def len_trigger(cls, text: str) -> int:  # type: ignore
         for trigger in cls.get_list_of_triggers():
-            if text.startswith(trigger):
+            if text.lower().startswith(trigger):
                 return len(trigger) + 1
 
     @classmethod
     def is_toxic(cls, text: str) -> bool:
         for trigger in cls.list_of_toxic:
-            if text.startswith(trigger):
+            if text.lower().startswith(trigger):
                 return True
         return False
 
